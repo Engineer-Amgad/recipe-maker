44f57bc01d699a7b365d2054d2b677dfc3cb673b
Engineer-Amgad <amgadmalik@yahoo.com>
2020-03-11 04:25:33 -0400

    created the ingredient resource

--- db/migrate/20200311010120_create_users.rb
+++ db/migrate/20200311010120_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.text :user_name
+      t.text :username
+      t.text :password_digest
 
       t.timestamps
     end
