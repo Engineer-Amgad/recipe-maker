1854a8022307f58d78737f44a4b56e1d20f19925
Engineer-Amgad <amgadmalik@yahoo.com>
2020-03-17 20:18:29 -0400

    added associations in the models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ApplicationRecord
-    has_many :recipes
-    has_many :users, through: :recipes
+    has_many :recipe_ingredients
+    has_many :recipes, through: :recipe_ingredients
 
+    validates_presence_of :name
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ApplicationRecord
     belongs_to :user
+    has_many :recipe_ingredients
+    has_man :ingredients, through: :recipe_ingredients
+    
     validates_presence_of :name
 
 end
