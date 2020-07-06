cd4576ff33357511becbb9a6b47a07d9ba051336
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-06 12:25:24 -0400

    creating sessions controller for user login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
+  get '/login' => "sessions#new"
+  post '/sessions' => "sessions#create"
+  
+  resources :users, only: [:new, :create]
+
   resources :recipes, only: [:index, :new, :create, :show] do
      resources :ingredients, only: [:index, :new]
   end
   
-  resources :users, only: [:new, :create]
+  
 
 end
