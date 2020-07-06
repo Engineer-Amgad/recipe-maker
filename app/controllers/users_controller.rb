39ef289db15d6cb92c96ad34d4d0e2953a5b4e4f
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-06 11:28:48 -0400

    added error validation message to user signup form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def new 
 
         @user = User.new
-        
+
     end 
 
     def create
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
         @user = User.new
         @user.username = params[:user][:username]
         if @user.save
-            redirect_to recipe_path
+            redirect_to recipes_path
         else
             render 'users/new'
         end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :recipes
+
+    validates :username, :presence => true
+    validates :username, :uniqueness => true
 end
