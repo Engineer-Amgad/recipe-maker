e913ee7cfd550369e039b16b35d92db0f660b903
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-06 10:48:37 -0400

    built the create method for user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
 
     def new 
+
         @user = User.new
+        
     end 
 
     def create
+
+        @user = User.new
+        @user.username = params[:user][:username]
+        if @user.save
+            redirect_to recipe_path
+        else
+            render 'users/new'
+        end 
 
     end 
 end
