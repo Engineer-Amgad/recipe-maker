a933c078f3e270f106702049b261f5787f854ce3
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-06 12:37:29 -0400

    created login form.

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
 
   get '/login' => "sessions#new"
   post '/sessions' => "sessions#create"
-  
+
   resources :users, only: [:new, :create]
 
   resources :recipes, only: [:index, :new, :create, :show] do
      resources :ingredients, only: [:index, :new]
   end
-  
-  
 
 end
