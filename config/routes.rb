48920f36daf16615627318f50337cf8ced2f2fa9
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-06 10:44:04 -0400

    creating a user signup feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   resources :recipes, only: [:index, :new, :create, :show] do
-    # resources :ingredients, only: [:index, :new]
+     resources :ingredients, only: [:index, :new]
   end
- 
+  
+  resources :users, only: [:new, :create]
+
 end
