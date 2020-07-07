59617848cb989cbcb6a9a8438323dcba232263a0
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-06 22:05:53 -0400

    completed signup, login and logout features.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,12 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(:username => params[:username])
-        session[:user_id] = user.id
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to recipes_path
+        else 
+            render 'sessions/new'
+        end 
     end 
 
     def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController < ApplicationController
 
         @user = User.new
         @user.username = params[:user][:username]
+        @user.password = params[:user][:password]
         if @user.save
+            session[:user_id] = @user.id
             redirect_to recipes_path
         else
             render 'users/new'
