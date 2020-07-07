ed67728822afdc6809241028cd9a77deab5927ee
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-06 21:12:13 -0400

    added links on the home page for sign up and login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,12 @@ class SessionsController < ApplicationController
     end 
 
     def create
-        user = User.find_by(:email => params[:username])
+        user = User.find_by(:username => params[:username])
         session[:user_id] = user.id
+    end 
+
+    def destroy
+        reset_session
+        redirect_to root_path
     end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'application#index'
   get '/login' => "sessions#new"
   post '/sessions' => "sessions#create"
-
+  get '/logout' => "sessions#destroy"
+  
   resources :users, only: [:new, :create]
 
   resources :recipes, only: [:index, :new, :create, :show] do
