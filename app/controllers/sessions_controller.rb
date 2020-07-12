38eccf6ce49eae1fe37e973884b2f3151ec1f31a
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-12 12:44:03 -0400

    created google oauth.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,53 @@ class SessionsController < ApplicationController
 
     end 
 
+    # def create
+    #     user = User.find_by(:username => params[:username])
+    #     if user && user.authenticate(params[:password])
+    #         session[:user_id] = user.id
+    #         redirect_to recipes_path
+    #     else 
+    #         render 'sessions/new'
+    #     end 
+    # end 
+
+    # def destroy
+    #     reset_session
+    #     redirect_to root_path
+    # end 
+
     def create
-        user = User.find_by(:username => params[:username])
-        if user && user.authenticate(params[:password])
+        if request.env["omniauth.auth"].present?
+          user = User.find_or_initialize_by(name: request.env["omniauth.auth"]['info']['name'])
+          if user.id.present?
             session[:user_id] = user.id
-            redirect_to recipes_path
-        else 
-            render 'sessions/new'
-        end 
-    end 
-
-    def destroy
-        reset_session
-        redirect_to root_path
-    end 
+            redirect_to user_path(user), notice: "You have successfully logged in"
+          else
+            user.password = SecureRandom.hex
+            user.save
+            session[:user_id] = user.id
+            redirect_to user_path(user), notice: "You have successfully logged in"
+          end
+        end
+        
+        if params[:name].present?
+          user = User.find_by_name(params[:name])
+          if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user), notice: "You have successfully logged in"
+          else
+           if user
+              flash[:alert] = "Your password is not correct"
+            else 
+              flash[:alert] = "The username is invalid"
+           end
+            render 'new'
+          end
+        end
+      end
+    
+      def destroy
+        session.clear
+        redirect_to '/', notice: "You have successfully logged out"
+      end
 end
-
-# google oauth
-# 713712716236-a8bcref2tpdn9fa8ekcom3v0rpnsv8jr.apps.googleusercontent.com
-# 6u7cowTB_4CI7hZjYDMfAaoV
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/login' => "sessions#new"
   post '/sessions' => "sessions#create"
   get '/logout' => "sessions#destroy"
-  
+  get '/auth/google_oauth2/callback' => 'sessions#create'
   resources :users, only: [:new, :create]
 
   resources :recipes, only: [:index, :new, :create, :show] do
