02000ed8555e3cb1119d30b5c1da943b8510720f
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-12 13:32:34 -0400

    completed oauth feature.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,36 +21,36 @@ class SessionsController < ApplicationController
 
     def create
         if request.env["omniauth.auth"].present?
-          user = User.find_or_initialize_by(name: request.env["omniauth.auth"]['info']['name'])
+            user = User.find_or_initialize_by(username: request.env["omniauth.auth"]['info']['name'])
           if user.id.present?
             session[:user_id] = user.id
-            redirect_to user_path(user), notice: "You have successfully logged in"
+            redirect_to recipes_path
           else
             user.password = SecureRandom.hex
             user.save
             session[:user_id] = user.id
-            redirect_to user_path(user), notice: "You have successfully logged in"
+            redirect_to recipes_path
           end
         end
         
-        if params[:name].present?
-          user = User.find_by_name(params[:name])
-          if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            redirect_to user_path(user), notice: "You have successfully logged in"
-          else
-           if user
-              flash[:alert] = "Your password is not correct"
-            else 
-              flash[:alert] = "The username is invalid"
-           end
-            render 'new'
-          end
+        if params[:username].present?
+          user = User.find_by_name(params[:username])
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id
+                redirect_to recipes_path
+            else
+                if user
+                    flash[:alert] = "Your password is not correct"
+                else 
+                    flash[:alert] = "The username is invalid"
+                end
+                render 'sessions/new'
+            end
         end
-      end
+    end
     
-      def destroy
-        session.clear
-        redirect_to '/', notice: "You have successfully logged out"
-      end
+    def destroy
+        reset_session
+        redirect_to root_path
+    end
 end
